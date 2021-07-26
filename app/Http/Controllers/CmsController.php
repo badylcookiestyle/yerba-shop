44e32b2b9741f7703fc2ceaf2e147c9ccfe83115
badylcookiestyle <73366240+badylcookiestyle@users.noreply.github.com>
2021-07-26 16:45:31 +0200

    just removed useless commit

---
+++ app/Http/Controllers/CmsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CmsController extends Controller
+{
+    public function editMain(Request $request){
+
+    }
+    public function editShop(Request $request){
+
+    }
+    public function editAbout(Request $request){
+
+    }
+}
