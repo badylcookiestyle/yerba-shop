d098245280ee4ce47d65789dcaffdfa5a5fff4b9
badylcookiestyle <73366240+badylcookiestyle@users.noreply.github.com>
2021-08-05 00:01:35 +0200

    route improvement

--- routes/web.php
+++ routes/web.php
@@ -24,60 +24,74 @@ Route::get('/about', function () {
 });
 
 //--- Payment
-
-Route::get('/payment',[App\Http\Controllers\PaymentController::class,'index']);
-Route::post('/payment',[App\Http\Controllers\PaymentController::class,'payment']);
+Route::prefix('payment')->group(function () {
+    Route::get('/', [App\Http\Controllers\PaymentController::class, 'index']);
+    Route::post('/', [App\Http\Controllers\PaymentController::class, 'payment']);
+});
 Auth::routes();
 //--- Admin dashboard
-Route::get('/admin',[App\Http\Controllers\AdminDashboardController::class,'index'])
-    ->name('admin.dashboard');
-Route::get('/admin/productList',[App\Http\Controllers\AdminDashboardController::class,'productList'])
-    ->name('admin.productList');
-Route::get('/admin/orders',[App\Http\Controllers\AdminDashboardController::class,'orders'])
-    ->name('admin.orders');
-Route::get('/admin/userList',[App\Http\Controllers\AdminDashboardController::class,'userList'])
-    ->name('admin.userList');
+Route::prefix('admin')->group(function () {
+    Route::get('/', [App\Http\Controllers\AdminDashboardController::class, 'index'])
+        ->name('admin.dashboard');
+    Route::get('/productList', [App\Http\Controllers\AdminDashboardController::class, 'productList'])
+        ->name('admin.productList');
+    Route::get('/orders', [App\Http\Controllers\AdminDashboardController::class, 'orders'])
+        ->name('admin.orders');
+    Route::get('/userList', [App\Http\Controllers\AdminDashboardController::class, 'userList'])
+        ->name('admin.userList');
+});
 //--- Home
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home');
 //--- Settings
-Route::get('/settings',[App\Http\Controllers\SettingsController::class,'index']);
+
+Route::prefix('settings')->group(function () {
+    Route::get('/', [App\Http\Controllers\SettingsController::class, 'index']);
+});
 //--- Shop Routes
 Route::get('/shop', [App\Http\Controllers\ShopController::class, 'index'])
     ->name('shop');
 //--- Product Routes
-Route::get('/product/{id}', [App\Http\Controllers\ProductController::class,'index'])
-    ->name('current.product');
-Route::post('/product', [App\Http\Controllers\ProductController::class,'add'])
-    ->name('add.product')
-    ->middleware("auth");
-Route::put('/product', [App\Http\Controllers\ProductController::class,'edit'])
-    ->name('edit.product')
-    ->middleware("auth");
-Route::delete('/product/{id}', [App\Http\Controllers\ProductController::class,'delete'])
-    ->name('delete.product');
+Route::prefix('product')->group(function () {
+    Route::get('/{id}', [App\Http\Controllers\ProductController::class, 'index'])
+        ->name('current.product');
+    Route::post('/', [App\Http\Controllers\ProductController::class, 'add'])
+        ->name('add.product')
+        ->middleware("auth");
+    Route::put('/', [App\Http\Controllers\ProductController::class, 'edit'])
+        ->name('edit.product')
+        ->middleware("auth");
+    Route::delete('/{id}', [App\Http\Controllers\ProductController::class, 'delete'])
+        ->name('delete.product');
+});
 //--- Cart Controller
-Route::get('/cart', [App\Http\Controllers\CartController::class,'index'])
-    ->name('current.cart');
-Route::post('/cart', [App\Http\Controllers\CartController::class,'addItem'])
-    ->name('add.item');
+Route::prefix('cart')->group(function () {
+    Route::get('/', [App\Http\Controllers\CartController::class, 'index'])
+        ->name('current.cart');
+    Route::post('/', [App\Http\Controllers\CartController::class, 'addItem'])
+        ->name('add.item');
 //btw it's not deleting a cart it's just deleting a category
-Route::delete('/cart/{id}', [App\Http\Controllers\CartController::class,'deleteItem'])
-    ->name('delete.item');
-Route::patch('/cart', [App\Http\Controllers\CartController::class,'editItem'])
-    ->name('edit.item');
-
+    Route::delete('/{id}', [App\Http\Controllers\CartController::class, 'deleteItem'])
+        ->name('delete.item');
+    Route::patch('/', [App\Http\Controllers\CartController::class, 'editItem'])
+        ->name('edit.item');
+});
 //--- Cms
-Route::get('admin/cms',[App\Http\Controllers\AdminDashboardController::class,'cms']);
-Route::post('cms/main',[App\Http\Controllers\CmsController::class,'editMain']);
-Route::post('cms/shop',[App\Http\Controllers\CmsController::class,'editShop']);
-Route::post('cms/about',[App\Http\Controllers\CmsController::class,'editAbout']);
-//--- Another Routes
-Route::delete('/user/{email}', [App\Http\Controllers\AdminDashboardController::class,'deleteUser'])
+Route::get('admin/cms', [App\Http\Controllers\AdminDashboardController::class, 'cms']);
+Route::prefix('cms')->group(function () {
+    Route::post('/main', [App\Http\Controllers\CmsController::class, 'editMain']);
+    Route::post('/shop', [App\Http\Controllers\CmsController::class, 'editShop']);
+    Route::post('/about', [App\Http\Controllers\CmsController::class, 'editAbout']);
+});
+//--- orders
+Route::prefix('order')->group(function () {
+    Route::get('/{id}', [App\Http\Controllers\PaymentController::class, 'getOrders']);
+    Route::patch('/', [App\Http\Controllers\PaymentController::class, 'changeStatus']);
+});
+//---Another Routes
+Route::delete('/user/{email}', [App\Http\Controllers\AdminDashboardController::class, 'deleteUser'])
     ->name('delete.user');
-Route::get('/order/{id}',[App\Http\Controllers\PaymentController::class,'getOrders']);
-Route::get('/details/{id}',[App\Http\Controllers\PaymentController::class,'details']);
-Route::patch('order',[App\Http\Controllers\PaymentController::class,'changeStatus']);
+Route::get('/details/{id}', [App\Http\Controllers\PaymentController::class, 'details']);
 //--- Search
-Route::post('search',[App\Http\Controllers\SearchController::class,'search']);
+Route::post('search', [App\Http\Controllers\SearchController::class, 'search']);
 
